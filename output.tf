20eea20d3e8ba9c491f3bbf0ceea4970762b4f50
iquzart <iquzart@hotmail.com>
2021-02-01 11:38:48 +0400

    restructure

--- output.tf
+++ output.tf
@@ -1,1 +1,13 @@
-# Azure App service Plan
+# Azure App service
+
+output "app_service_name" {
+  description = "Name of the App Service"
+  value       = azurerm_app_service.app.name
+}
+
+output "app_service_default_site_hostname" {
+  description = "The Default Hostname associated with the App Service"
+  value       = azurerm_app_service.app.default_site_hostname
+}
+
+
